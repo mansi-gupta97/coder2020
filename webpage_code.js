3ba904f545cbbe459030837b93112a91d2bcb456
mansi19972020 <gmansi908@gmail.com>
2021-07-02 23:31:02 +0530

    need to complete

---
+++ webpage_code.js
@@ -0,0 +1,20 @@
+<html>
+<body>
+<script language="javascript" type="text/javascript">
+var a;
+a =prompt("enter the number:");
+switch(a)
+{
+case 0:
+document.write("Sunday");
+break;
+case 1:
+document.write("Monday");
+break;
+default:
+document.write("not day");
+}
+</script>
+</body>
+</html>
+ 
